542377323b1dc5e95c64f9b6ef4ecfcbf497aa09
dstepbw <dstepbw@gmail.com>
2016-11-18 13:48:37 -0200

    up

---
+++ src/popuuid4/activator/Activador.java
@@ -0,0 +1,19 @@
+package popuuid4.activator;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activador implements BundleActivator {
+
+	@Override
+	public void start(BundleContext arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stop(BundleContext arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
